23a25002021028c2592d0d0b4dded14d25907d0f
Evan Hahn <me@evanhahn.com>
2014-07-21 17:51:34 -0700

    omg it works

--- public/the.js
+++ public/the.js
@@ -25,49 +25,48 @@ $(function() {
     $iframeland.children().remove();
 
     var usernames = $input.val().split(/\s|,/g);
-    var leftToLoad = usernames.length;
 
-    var iframeLoaded = function() {
-      leftToLoad --;
-      if (leftToLoad === 0) {
-
-        var ajax = $.get("/results", { id: id }, function(data) {
-
-          $userlist.children().remove();
-
-          var positives = 0;
-
-          $.each(data, function(username, isLoggedIn) {
-            var li = document.createElement("li");
-            if (isLoggedIn) {
-              var body = document.createElement("strong");
-              body.innerHTML = username + " is logged in";
-              li.appendChild(body);
-              positives ++;
-            } else {
-              li.innerHTML = username + " is not logged in";
-            }
-            $userlist.append(li);
-          });
-
-        });
-
-        ajax.always(function() {
-          $submit.attr("disabled", false);
-        });
-
-      }
-    };
-
-    usernames.forEach(function(username) {
-
+    var currentIndex = 0;
+    var next = function() {
       var iframe = document.createElement("iframe");
-      iframe.src = "/test?id=" + id + "&user=" + username;
-      iframe.onload = iframeLoaded;
+      iframe.src = "/test?id=" + id + "&user=" + usernames[currentIndex];
       iframe.style.display = "none";
+      iframe.onload = function() {
+        $(iframe).remove();
+        currentIndex ++;
+        if (currentIndex < usernames.length) {
+          next();
+        } else {
+          done();
+        }
+      };
       $iframeland.append(iframe);
+    };
+    next();
 
-    });
+    var done = function() {
+      var ajax = $.get("/results", { id: id }, function(data) {
+
+        $userlist.children().remove();
+
+        $.each(data, function(username, isLoggedIn) {
+          var li = document.createElement("li");
+          if (isLoggedIn) {
+            var body = document.createElement("strong");
+            body.innerHTML = username + " is logged in";
+            li.appendChild(body);
+          } else {
+            li.innerHTML = username + " is not logged in";
+          }
+          $userlist.append(li);
+        });
+
+      });
+
+      ajax.always(function() {
+        $submit.attr("disabled", false);
+      });
+    };
 
     $submit.attr("disabled", true);
 
